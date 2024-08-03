614bd4b009f15466125681c63b16754985cc8d37
kavinda-100 <kavindarathnayake100@gmail.com>
2024-08-03 19:39:55 +0530

    add prisma to project and create prisma client and create the models for User and Account and sync with the mongodb database.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import {cn} from "../lib/utils";
 import React from "react";
+import prismaDB from "../lib/prismaDB";
 
 const inter = Inter({ subsets: ["latin"] });
 
