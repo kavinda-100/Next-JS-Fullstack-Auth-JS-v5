43daead8f02d0cbb83701a319147ae14395ff1c9
kavinda-100 <kavindarathnayake100@gmail.com>
2024-08-05 11:43:12 +0530

    add UserRole to the prisma and update the db.

--- auth.ts
+++ auth.ts
@@ -4,6 +4,26 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import prismaDB from "./lib/prismaDB";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+    callbacks:{
+        async session({session, token}){
+            //check if session has user and token has sub
+            if(session.user && token.sub){
+                /**
+                 * Add the user id to the session which in token.sub
+                 * by doing that we can access the user id in the session which mean we can access
+                 * the user id in both sever and client side/component
+                 * because by default session not include the user id
+                 * */
+                session.user.id = token.sub
+            }
+            console.log("session", session)
+            console.log("session-token", token)
+            return session
+        },
+        async jwt({token}){
+            return token
+        }
+    },
     adapter: PrismaAdapter(prismaDB),
     session: { strategy: "jwt" },
     ...authConfig,
